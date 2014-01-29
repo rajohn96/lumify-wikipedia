98eb3ecc2feefe6ce7af4d706bb0ec7cd9378ca6
Joe Ferner <joe@fernsroth.com>
2014-01-29 09:23:26 -0500

    added noflush option to wikipedia storm

--- src/main/java/com/altamiracorp/lumify/wikipedia/WikipediaConstants.java
+++ src/main/java/com/altamiracorp/lumify/wikipedia/WikipediaConstants.java
@@ -2,4 +2,5 @@ package com.altamiracorp.lumify.wikipedia;
 
 public class WikipediaConstants {
     public static final String WIKIPEDIA_QUEUE = "wikipedia";
+    public static final String CONFIG_FLUSH = "flush";
 }
