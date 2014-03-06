81e6095af29412973eeb6041a4a475dcc100849e
David Singley <dsingley@nearinfinity.com>
2014-03-06 12:00:27 -0500

    fix classpath

--- bin/importOntology.sh
+++ bin/importOntology.sh
@@ -8,7 +8,7 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-classpath=$(${DIR}/classpath.sh .)
+classpath=$(${DIR}/classpath.sh lumify-wikipedia-core)
 if [ $? -ne 0 ]; then
   echo "${classpath}"
   exit
