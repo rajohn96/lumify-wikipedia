4d06699dcd3030d6ebe511a82eb697f5df0cd51e
David Singley <dsingley@nearinfinity.com>
2014-06-03 15:15:46 -0400

    add splits for 0-9

--- bin/configure_splits.sh
+++ bin/configure_splits.sh
@@ -4,7 +4,7 @@ function _write_split_file {
   local prefix=$1
   local filename=$(mktemp -t $(basename $0).XXX)
 
-  for a in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+  for a in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z; do
     echo "${prefix}${a}" >> ${filename}
   done
 
